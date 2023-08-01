030acf7fd851806acb4e204e39a28b8c0a149014
Manuel García Criado (laptop/linux) <manuel.garcia.cr@hotmail.com>
2023-08-01 18:46:10 +0200

    Exercise 4 Routing
     En la rama main
     Tu rama está actualizada con 'origin/main'.
    
     Cambios a ser confirmados:
    	nuevos archivos: .vscode/settings.json
    	modificados:     src/app/app-routing.module.ts
    	modificados:     src/app/app.module.ts
    	modificados:     src/app/home/home.component.html
    	nuevos archivos: src/app/landing/landing.component.html
    	nuevos archivos: src/app/landing/landing.component.scss
    	nuevos archivos: src/app/landing/landing.component.spec.ts
    	nuevos archivos: src/app/landing/landing.component.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LandingComponent } from './landing/landing.component';
 
 const routes: Routes = [
-    { path: 'home', component: HomeComponent },
+    { path: 'home', component: LandingComponent },
+    { path: 'calculator', component: HomeComponent },
     { path: '',   redirectTo: '/home', pathMatch: 'full' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { PanelComponent } from './panel/panel.component';
+import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         HomeComponent,
-        PanelComponent
+        PanelComponent,
+        LandingComponent
     ],
     imports: [
         BrowserModule,
